c50133b9c26d183cf2108d5cfbc6e8deac8a40a0
LJM12914 <LJM12914@outlook.com>
2025-03-02 00:07:25 +0800

    666666666

--- src/components/Forum.tsx
+++ src/components/Forum.tsx
@@ -1,10 +1,31 @@
-﻿import { useCheckLoginNav } from "../hooks/useCheckLoginNav";
-
+﻿import { useContext, useEffect, useState } from "react";
+import { useCheckLoginNav } from "../hooks/useCheckLoginNav";
+import meta from "../meta";
+import { MessageContext } from "./App";
+import { ForumLoginResponse } from "../schema/forumLogin";
 
 export default function Forum(){
-    const { accessToken, loggedIn } = useCheckLoginNav();
+    const { accessToken, loggedIn, initialzing, setLogin } = useCheckLoginNav();
+    const { messageApi } = useContext(MessageContext);
+    const [src, setSrc] = useState("");
+    useEffect(()=>{(async()=>{
+        if(!initialzing){
+            const response = fetch(`${meta.apiDomain}/v1/status/getForumToken`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${accessToken}`
+                }
+            });
+            response.catch(e=>{messageApi!.error(JSON.stringify(e))});
+            const data :ForumLoginResponse = await (await response).json();
+            console.log(data);
+            if(data.success) setSrc(`https://scmbbs.wjlo.cc/token-login?code=${data.forum_token}`);
+            else messageApi!.error("登录论坛时出现错误！");
+        }
+    })()}, [initialzing]);
     return(<div className="grow">
-        <iframe className="h-full w-full" src="https://scmbbs.wjlo.cc">
+        <iframe seamless className="h-full w-full" src={src}>
         </iframe>
     </div>);
 }
---
+++ src/schema/forumLogin.ts
@@ -0,0 +1,6 @@
+﻿
+//GET `/v1/status/getForumToken`
+export type ForumLoginResponse = {
+    success :boolean;
+    forum_token :string;
+};
